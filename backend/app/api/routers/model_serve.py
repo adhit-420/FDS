fab69b73eb9b5e17fe4a10c2886341b406a76831
pupper emeritus <pupperemeritus9@protonmail.com>
2024-04-27 02:14:13 +0530

    added proba

--- backend/app/api/routers/model_serve.py
+++ backend/app/api/routers/model_serve.py
@@ -1,9 +1,25 @@
 from fastapi import APIRouter, Depends
 from app.api.models.phish_model import RFModel
+from pydantic import BaseModel
+
 
 router = APIRouter()
 
 
+# Define the input data model
+class InputData(BaseModel):
+    url: str
+
+
+# Define the output data model
+class OutputData(BaseModel):
+    prediction: int
+    probabilities: int
+
+
 @router.get("/phish_model")
 async def predict_phish_model(url: str):
-    return RFModel().predict([url])[0]
+    model = RFModel()
+    pred = model.predict([url])
+    proba = model.predict_proba([url])
+    return {pred: pred, proba: proba}
