1ca87bfcbb0d5fdbcf9dfb2aaa527a7b00cb9d4e
pupper emeritus <pupperemeritus9@protonmail.com>
2024-04-27 07:18:42 +0530

    fixed everything

--- frontend/script.js
+++ frontend/script.js
@@ -2,38 +2,42 @@
 async function fetchData() {
     try {
         // Get the current URL of the page
-        const currentURL = window.location.href;
-        
+        let activeTab = await chrome.tabs.query({
+            active: true,
+            lastFocusedWindow: true,
+        });
+        let currentURL = activeTab[0].url;
+        const domain = new URL(currentURL).hostname;
+        console.log(domain);
         // Make a GET request to your FastAPI endpoint with the current URL as input
-       const res = await fetch(`http://localhost:8000/phish_model?url=${encodeURIComponent(currentURL)}`);
-       //const youtubeURL = 'https://www.youtube.com'; // URL of YouTube
-       //const res = await fetch(`http://localhost:8000/phish_model?url=${encodeURIComponent(youtubeURL)}`);
+        let res = await fetch(
+            `http://localhost:8000/phish_model?url=${domain}`
+        );
+        //const youtubeURL = 'https://www.youtube.com'; // URL of YouTube
+        //const res = await fetch(`http://localhost:8000/phish_model?url=${encodeURIComponent(youtubeURL)}`);
         if (!res.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error("Network response was not ok");
         }
 
         const record = await res.json();
+        console.log(record);
         // Update HTML elements with fetched data
         document.getElementById("Suspicion").innerHTML = record.proba;
         document.getElementById("Rag").innerHTML = record.pred;
     } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
+        console.error("There was a problem with the fetch operation:", error);
     }
 }
 
-// Call fetchData function when the page loads
-window.onload = function () {
+// Call fetchData function when the page reloads
+window.onload = window.onbeforeunload = function () {
     fetchData();
 };
-
-
-// Call fetchData function when the page loads
-window.onload = function () {
-    fetchData();
-};
-document.addEventListener('DOMContentLoaded', function() {
-    var reportButton = document.getElementById('reportButton');
-    reportButton.addEventListener('click', function() {
-        chrome.tabs.create({ url: 'https://www.cybercrime.gov.in/Webform/Accept.aspx' });
+document.addEventListener("DOMContentLoaded", function () {
+    var reportButton = document.getElementById("reportButton");
+    reportButton.addEventListener("click", function () {
+        chrome.tabs.create({
+            url: "https://www.cybercrime.gov.in/Webform/Accept.aspx",
+        });
     });
-})
+});
