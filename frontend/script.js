5dbde37b425f20f2c2817e1423c40be27c9130e9
Adhit <simadi2004@gmail.com>
2024-04-27 06:41:36 +0530

    integrated front end and backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,36 @@
+// async function to fetch data from the API endpoint and update HTML elements
 async function fetchData() {
-    const res=await fetch ("");
-    const record=await res.json();
-    document.getElementById("Suspicion").innerHTML=record.data[0].suspicionScore;
-    document.getElementById("Rag").innerHTML=record.data[0].rag;
-fetchData();
+    try {
+        // Get the current URL of the page
+        const currentURL = window.location.href;
+        
+        // Make a GET request to your FastAPI endpoint with the current URL as input
+       const res = await fetch(`http://localhost:8000/phish_model?url=${encodeURIComponent(currentURL)}`);
+       //const youtubeURL = 'https://www.youtube.com'; // URL of YouTube
+       //const res = await fetch(`http://localhost:8000/phish_model?url=${encodeURIComponent(youtubeURL)}`);
+        if (!res.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const record = await res.json();
+        // Update HTML elements with fetched data
+        document.getElementById("Suspicion").innerHTML = record.proba;
+        document.getElementById("Rag").innerHTML = record.pred;
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
 }
 
+// Call fetchData function when the page loads
+window.onload = function () {
+    fetchData();
+};
+
+
+// Call fetchData function when the page loads
+window.onload = function () {
+    fetchData();
+};
 document.addEventListener('DOMContentLoaded', function() {
     var reportButton = document.getElementById('reportButton');
     reportButton.addEventListener('click', function() {
